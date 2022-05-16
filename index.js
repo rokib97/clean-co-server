2ec7f3c268928d0f170b81ab61c42e9eb362f506
rokib97 <hasanrokib1997@gmail.com>
2022-05-16 21:12:56 +0600

    add simple get api to get the services from database

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.errmo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const serviceCollection = client.db("CleanCo").collection("service");
+
+    app.get("/service", async (req, res) => {
+      const services = await serviceCollection.find({}).toArray();
+      res.send(services);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
   res.send("Hello from Clean and Co");
 });
 
