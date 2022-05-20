48fe249a9ca3336639ac2f0a46812cb88de13e49
rokib97 <hasanrokib1997@gmail.com>
2022-05-20 18:02:43 +0600

    added update and delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -41,6 +41,29 @@ async function run() {
     app.post("/add-service", async (req, res) => {
       const data = req.body;
       const result = await serviceCollection.insertOne(data);
+      res.send(result);
+    });
+    // update service
+    app.put("/update-service/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: data,
+      };
+      const result = await serviceCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+    // delete a service api
+    app.delete("/delete-service/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
